d821b218ee17649791044377076af8d3bfa74cb0
Michal Štván <michalstvan@gmail.com>
2024-10-06 20:39:38 +0200

    change paths

--- routes/probes.php
+++ routes/probes.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 Route::prefix(config('probes.prefix'))->group(function() {
-    Route::get('/probe/liveness', function () {
+    Route::get('/probe/live', function () {
         return response()->json(['status' => 'ok']);
     });
 
-    Route::get('/probe/readiness', function () {
+    Route::get('/probe/ready', function () {
         DB::connection()->getPdo();
         Cache::put('readiness_test', 1, 1);
 
